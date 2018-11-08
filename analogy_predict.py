c49bc2539dcf650878351912919664b23c7dcedd
iamaziz <iamaziz.alto@gmail.com>
2018-11-08 16:10:25 -0500

    Tiny change for Python3 and fix typo in readme

--- analogy_predict.py
+++ analogy_predict.py
@@ -14,13 +14,13 @@ model = kv.load_word2vec_format(TRAINING + '_' + VERB_TYPE + '.txt.w2v')
 #     for line in f:
 #         triple = line[:-1].split(' ')
 #         props.add(triple[1])
-    
+
 uri2id = {'&': '-2', '^': '-1'}
 id2uri = {'-2': '&', '-1': '^'}
 with open(TRAINING + '_dictionary.txt') as f:
     for line in f:
         sp = line.find(' ')
-        line = [line[:sp], line[sp+1:-1]]
+        line = [line[:sp], line[sp + 1:-1]]
         id2uri[line[0]] = line[1]
         uri2id[line[1]] = line[0]
 
@@ -29,11 +29,13 @@ with open(TRAINING + '_output.txt') as data:
     for dline in data:
         training.add(dline)
 
+
 def is_in_training(s, p, o):
     string = "{} {} {}\n".format(s, p, o)
     if string in training:
         return True
     return False
+
 
 # c = 0
 with open(TEST + '_output.txt') as test:
@@ -42,7 +44,7 @@ with open(TEST + '_output.txt') as test:
         s_uri = id2uri[ttriple[0]]
         p_uri = id2uri[ttriple[1]]
         # print [id2uri[x] for x in ttriple]
-        
+
         # dict() object -> occurrences
         occur = dict()
         with open(TRAINING + '_output.txt') as data:
@@ -60,14 +62,14 @@ with open(TEST + '_output.txt') as test:
                         if obj not in occur:
                             occur[obj] = 0
                         occur[obj] += 1
-        
+
         # compute triples confidence
         maxval = None
-        for key, value in sorted(occur.iteritems(), key=lambda (k,v): (v,k), reverse=True)[:20]:
+        for key, value in sorted(occur.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)[:20]:
             if maxval is None:
                 maxval = value
-            print "{}\t<{}> <{}> <{}>".format(float(value) / maxval, s_uri, p_uri, id2uri[key])
-        
+            print("{}\t<{}> <{}> <{}>".format(float(value) / maxval, s_uri, p_uri, id2uri[key]))
+
         # c += 1
         # if c > 10:
         #     break
