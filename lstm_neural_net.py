2c95b7cda9ba2b3c27b876a9b6bec3187b1ce495
iamaziz <iamaziz.alto@gmail.com>
2019-01-13 21:24:10 -0500

    Refactoring: objectiviy
    wrap code in functions, namely:
    
    - load_embeddings()
    - read_training_data()
    - read_test_data()
    - load_dictionary()
    - negative_sampling()
    - build_modelbuild_model()
    
    TODO:
    - evaluation()

--- lstm_neural_net.py
+++ lstm_neural_net.py
@@ -22,118 +22,154 @@ numpy.random.seed(7)
 NEG_RATE = 1
 N_EPOCHS = int(epochs)
 
-X_train = list()
-y_train = list()
-vectors = dict()
-size = None
-with open(embeddings) as f:
-    for line in f:
-        line = line[:-1].split(' ')
-        if len(line) == 2:
-            continue
-        vectors[line[0]] = line[1:]
-        size = len(line[1:])
-print("Embeddings loaded.")
-sys.stdout.flush()
 
-training = dict()
-with open(train_triples) as f:
-    for line in f:
-        line = line[:-1].split(' ')
-        seq = [vectors[x] for x in line]
-        key = (line[0], line[1])
-        if key not in training:
-            training[key] = list()
-        training[key].append(line[2])
-        X_train.append(seq)
-        y_train.append(1.0)
-print("Training triples loaded and indexed.")
-sys.stdout.flush()
+# X_train = list()
+# y_train = list()
+# vectors = dict()
+# training = dict()
+# size = None
 
-X_test = list()
-y_test = list()
-idx_test = list()
-with open(test_triples) as f:
-    for line in f:
-        line = line[:-1].split(' ')
-        seq = [vectors[x] for x in line]
-        idx_test.append(line)
-        X_test.append(seq)
-        y_test.append(1.0)
-print("Test triples loaded and indexed.")
-sys.stdout.flush()
+# -- Load KG Embeddings
+def load_embeddings(embedding_file):
+    _vectors = dict()
+    _size = None
+    with open(embedding_file) as f:
+        for line in f:
+            line = line[:-1].split(' ')
+            if len(line) == 2:
+                continue
+            _vectors[line[0]] = line[1:]
+            _size = len(line[1:])
+    print("Embeddings loaded.")
+    sys.stdout.flush()
+    return _vectors, _size
 
+
+# -- Read training triplets
+def read_training_data(train_file):
+    _training = dict()
+    _X_train = list()
+    _y_train = list()
+    with open(train_file) as f:
+        for line in f:
+            line = line[:-1].split(' ')
+            seq = [vectors[x] for x in line]
+            key = (line[0], line[1])
+            if key not in _training:
+                _training[key] = list()
+            _training[key].append(line[2])
+            _X_train.append(seq)
+            _y_train.append(1.0)
+    print("Training triples loaded and indexed.")
+    sys.stdout.flush()
+    return _training, _X_train, _y_train
+
+
+# -- Read test triplets
+def read_test_data(test_file):
+    _X_test = list()
+    _y_test = list()
+    _idx_test = list()
+    with open(test_file) as f:
+        for line in f:
+            line = line[:-1].split(' ')
+            seq = [vectors[x] for x in line]
+            _idx_test.append(line)
+            _X_test.append(seq)
+            _y_test.append(1.0)
+    print("Test triples loaded and indexed.")
+    sys.stdout.flush()
+    return _X_test, _y_test, _idx_test
+
+
+# -- Load Dictionary: Entities/Relations <--> URI
+def load_dictionary(dict_file):
+    _id2uri = dict()
+    with open(dict_file) as f:
+        for line in f:
+            sp = line.find(' ')
+            line = [line[:sp], line[sp + 1:-1]]
+            _id2uri[line[0]] = line[1]
+    print("Dictionary loaded.")
+    return _id2uri
+
+
+vectors, size = load_embeddings(embedding_file=embeddings)
+training, X_train, y_train = read_training_data(train_file=train_triples)
+X_test, y_test, idx_test = read_test_data(test_file=test_triples)
 vkeys = list(vectors.keys())
-id2uri = dict()
-with open(dictionary) as f:
-    for line in f:
-        sp = line.find(' ')
-        line = [line[:sp], line[sp + 1:-1]]
-        id2uri[line[0]] = line[1]
-print("Dictionary loaded.")
-print("Negative sampling:", neg_sampling)
-sys.stdout.flush()
-if neg_sampling == "random":
+id2uri = load_dictionary(dict_file=dictionary)
 
-    tr_size = len(y_train)
-    lenv = len(vectors)
-    for i in range(NEG_RATE * tr_size):
-        idx0 = None
-        idx1 = None
-        idx2 = None
-        while True:
-            idx0 = vkeys[int(numpy.random.random() * lenv)]
-            if idx0.startswith("id_"):
-                break
-        while True:
-            idx1 = vkeys[int(numpy.random.random() * lenv)]
-            if idx1.startswith("pr_"):
-                break
-        if (idx0, idx1) not in training:
-            while True:
-                idx2 = vkeys[int(numpy.random.random() * lenv)]
-                if idx2.startswith("id_"):
-                    break
-        else:
-            while True:
-                idx2 = vkeys[int(numpy.random.random() * lenv)]
-                if idx2.startswith("id_") and idx2 not in training[(idx0, idx1)]:
-                    break
-        idx = [idx0, idx1, idx2]
-        # print [id2uri[x] for x in idx]
-        sys.stdout.flush()
 
-        seq = [vectors[idx[x]] for x in range(3)]
-        X_train.append(seq)
-        y_train.append(0.0)
-        if i % 1000 == 0:
-            print("{} negative triples sampled.".format(i + 1))
+# -- CONSTRUCT NEGATIVE EXAMPLES
+def negative_sampling():
+    print("Negative sampling:", neg_sampling)
+    sys.stdout.flush()
+    if neg_sampling == "random":
+
+        tr_size = len(y_train)
+        lenv = len(vectors)
+        for i in range(NEG_RATE * tr_size):
+            idx0 = None
+            idx1 = None
+            idx2 = None
+            while True:
+                idx0 = vkeys[int(numpy.random.random() * lenv)]
+                if idx0.startswith("id_"):
+                    break
+            while True:
+                idx1 = vkeys[int(numpy.random.random() * lenv)]
+                if idx1.startswith("pr_"):
+                    break
+            if (idx0, idx1) not in training:
+                while True:
+                    idx2 = vkeys[int(numpy.random.random() * lenv)]
+                    if idx2.startswith("id_"):
+                        break
+            else:
+                while True:
+                    idx2 = vkeys[int(numpy.random.random() * lenv)]
+                    if idx2.startswith("id_") and idx2 not in training[(idx0, idx1)]:
+                        break
+            idx = [idx0, idx1, idx2]
+            # print [id2uri[x] for x in idx]
             sys.stdout.flush()
 
-elif neg_sampling == "corrupt":
-
-    for key in training:
-        idx_orig = key
-        for j in range(len(training[key])):
-            added = set()
-            while len(added) < NEG_RATE:
-                random_obj = vkeys[int(numpy.random.random() * len(vectors))]
-                if random_obj not in training[key]:
-                    added.add(random_obj)
-            for random_obj in added:
-                idx = list(idx_orig)
-                idx.append(random_obj)
-                print([id2uri[x] for x in idx])
+            seq = [vectors[idx[x]] for x in range(3)]
+            X_train.append(seq)
+            y_train.append(0.0)
+            if i % 1000 == 0:
+                print("{} negative triples sampled.".format(i + 1))
                 sys.stdout.flush()
 
-                seq = [vectors[idx[x]] for x in range(3)]
-                X_train.append(seq)
-                y_train.append(0.0)
+    elif neg_sampling == "corrupt":
 
-else:
-    # will probably fail for having no negatives
-    print("+++ WARNING: no negatives generated! +++")
-    sys.stdout.flush()
+        for key in training:
+            idx_orig = key
+            for j in range(len(training[key])):
+                added = set()
+                while len(added) < NEG_RATE:
+                    random_obj = vkeys[int(numpy.random.random() * len(vectors))]
+                    if random_obj not in training[key]:
+                        added.add(random_obj)
+                for random_obj in added:
+                    idx = list(idx_orig)
+                    idx.append(random_obj)
+                    print([id2uri[x] for x in idx])
+                    sys.stdout.flush()
+
+                    seq = [vectors[idx[x]] for x in range(3)]
+                    X_train.append(seq)
+                    y_train.append(0.0)
+
+    else:
+        # will probably fail for having no negatives
+        print("+++ WARNING: no negatives generated! +++")
+        sys.stdout.flush()
+
+
+negative_sampling()
+# -- END OF NEGATIVE SAMPLES
 
 print("dimensions:", size)
 print("train size:", len(X_train))
@@ -149,6 +185,7 @@ y_train = numpy.asarray(y_train)
 X_test = numpy.asarray(X_test)
 y_test = numpy.asarray(y_test)
 
+
 # class TestCallback(Callback):
 #     def __init__(self, test_data):
 #         self.test_data = test_data
@@ -158,15 +195,20 @@ y_test = numpy.asarray(y_test)
 #         loss, acc = self.model.evaluate(x, y, verbose=0)
 #         print('\nTesting loss: {}, acc: {:.2f}\n'.format(loss, acc*100))
 
-# create the model
-model = Sequential()
-model.add(LSTM(size, input_shape=(3, size)))  # , return_sequences=True))
-# model.add(LSTM(size))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='mean_squared_error', optimizer='adam')  # , metrics=['accuracy'])
-print(model.summary())
-sys.stdout.flush()
-model.fit(X_train, y_train, epochs=N_EPOCHS, batch_size=16)  # , callbacks=[TestCallback((X_test, y_test))])
+# -- Create Keras model
+def build_model():
+    model = Sequential()
+    model.add(LSTM(size, input_shape=(3, size)))  # , return_sequences=True))
+    # model.add(LSTM(size))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='mean_squared_error', optimizer='adam')  # , metrics=['accuracy'])
+    print(model.summary())
+    sys.stdout.flush()
+    model.fit(X_train, y_train, epochs=N_EPOCHS, batch_size=16)  # , callbacks=[TestCallback((X_test, y_test))])
+    return model
+
+
+model = build_model()
 
 # # # # Final evaluation of the model
 # for i in range(len(y_test)):
